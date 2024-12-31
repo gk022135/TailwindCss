4011e409aa42e3d0a1c304171dd7851704b3704d
gk022135 <gk022135@gmail.com>
2024-12-31 22:50:18 +0530

    #

--- TailwindcssTut/src/App.jsx
+++ TailwindcssTut/src/App.jsx
@@ -1,5 +1,6 @@
 
 import './App.css'
+import Tutorial from './Tutorial'
 
 function App() {
 
@@ -9,7 +10,8 @@ function App() {
       <div className="h-screen flex items-center justify-center bg-blue-100">
         <h1 className="text-4xl font-bold text-blue-600">Hello, Tailwind CSS!</h1>
       </div>
-  
+      <Tutorial />
+
     </>
   )
 }
---
+++ TailwindcssTut/src/Tutorial.jsx
@@ -0,0 +1,16 @@
+function Tutorial(){
+    return(
+        <div>
+            <h1 className="text-4xl font-bold text-orange-500"> hi guys you going to lesrn Tailwind css</h1>
+            <h1 className="text-4xl font-bold text-blue-600">Hello, Tailwind CSS!</h1>
+
+
+
+            <h1 className="p-6 max-w-sm h-100 mx-auto rounded-xl bg-red-500 grid place-content-center text-3xl font-extrabold">make it center</h1>
+
+            <button className="bg-orange-500 w-40 h-15 rounded-2xl m-4 p-2 hover:bg-blue-500">buy now</button>
+        </div>
+    )
+};
+
+export default Tutorial;
