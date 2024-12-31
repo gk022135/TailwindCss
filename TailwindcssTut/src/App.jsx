09681ea09fae46a95a12b9aca109f860bab8fa2b
gk022135 <gk022135@gmail.com>
2024-12-31 22:09:11 +0530

    Tailwind Css Tutorial

---
+++ TailwindcssTut/src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <div className="h-screen flex items-center justify-center bg-blue-100">
+        <h1 className="text-4xl font-bold text-blue-600">Hello, Tailwind CSS!</h1>
+      </div>
+  
+    </>
+  )
+}
+
+export default App
